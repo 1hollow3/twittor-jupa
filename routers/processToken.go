5b9997a2938fec76dcee4e7fdf85d9ec28d2007d
1hollow3 <pablo.bernal@mercadolibre.com.co>
2021-12-27 14:32:22 -0500

    SeeProfile.1

--- routers/processToken.go
+++ routers/processToken.go
@@ -31,16 +31,16 @@ func ProcessToken(tk string) (*models.Claim, bool, string, error) {
 		return myKey, nil
 	})
 
-	if err == nil {
-		_, found, _ := db.UserExist(claims.Email)
-		if found == true {
-			Email = claims.Email
-			IDUser = claims.ID.Hex()
-		}
-		return claims, found, IDUser, nil
-	}
 	if !tkn.Valid {
 		return claims, false, string(""), errors.New("invalid token")
 	}
-
+	if err != nil {
+		return claims, false, string(""), errors.New("invalid token")
+	}
+	_, found, _ := db.UserExist(claims.Email)
+	if found == true {
+		Email = claims.Email
+		IDUser = claims.ID.Hex()
+	}
+	return claims, found, IDUser, nil
 }
